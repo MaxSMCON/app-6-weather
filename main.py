c9f3af1b403d92006c96900341cfbb4ba478e603
Maxim <smetmx@gmail.com>
2023-09-08 15:04:30 -0400

    Day 28 : initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# import pandas as pd
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/vi/<station>/<date>")
+def about(station, date):
+    # df = pd.read_csv("")
+    temperature = 23 #df.station(date)
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+if __name__ == "__main__":
+    app.run(debug=True)
